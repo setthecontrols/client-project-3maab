bd7c5c15afaaef1a4bc6165bd6c160a7fbd94ac3
Tony <saric.tony@gmail.com>
2017-03-30 15:03:40 -0700

    test

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,9 +4,21 @@ class UsersController < ApplicationController
   end
 
   def create
+    @user = User.new(user_params)
+
+    respond_to do |format|
+      if @user.save
+        format.html { redirect_to @user, notice: 'User was successfully created.' }
+        format.json { render :show, status: :created, location: @user }
+      else
+        format.html { render :new }
+        format.json { render json: @user.errors, status: :unprocessable_entity }
+      end
+    end
   end
 
   def new
+    @user = User.new
   end
 
   def show
@@ -14,5 +26,11 @@ class UsersController < ApplicationController
   end
 
   def update
+  end
+
+  private
+
+  def user_params
+     params.require(:user).permit(:first_name, :last_name, :email, :username, :password)
   end
 end
