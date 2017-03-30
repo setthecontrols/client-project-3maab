ffd5aa18ef3f82ceeac1c4cb7392ff54f7de90b1
Aaron Calander <aaroncalander@yahoo.com>
2017-03-30 15:41:13 -0700

    Create new recipe form and view for weekly menu

---
+++ app/controllers/menus_controller.rb
@@ -0,0 +1,20 @@
+class MenusController < ApplicationController
+  def index
+    @recipes = Recipe.all
+    @appetizers = Recipe.where(category: "appetizers")
+    @entrees = Recipe.where(category: "entrees")
+    @salads = Recipe.where(category: "salads")
+    @desserts = Recipe.where(category: "desserts")
+  end
+end
+
+
+private
+
+def rating_total
+  @appetizers.each do |appetizer|
+    app_ratings = appetizer.ratings
+    total = app_ratings.reduce(0) { |sum, rating| sum += rating.value }
+    return total
+  end
+end
