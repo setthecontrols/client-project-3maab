116ff5d566e935621977bab0a7b1ae698ca962b0
“angelamarshall” <hello@angelasmarshall.com>
2017-03-30 23:01:48 -0700

    fixes bug in registering

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,17 +6,13 @@ class UsersController < ApplicationController
 
   def create
     @user = User.new(user_params)
-
-    respond_to do |format|
-      if @user.save
-        format.html { redirect_to @user, notice: 'User was successfully created.' }
-        format.json { render :show, status: :created, location: @user }
-        login
-        render 'users/show'
-      else
-        format.html { render :new }
-        format.json { render json: @user.errors, status: :unprocessable_entity }
-      end
+    if @user.save
+      login
+      flash.notice = "Thanks for registering '#{@user.username}'!!!!"
+      redirect_to recipes_path
+    else
+      flash.notice = "We were unable to sign you up... please try again."
+      redirect_to '/users/new'
     end
   end
 
