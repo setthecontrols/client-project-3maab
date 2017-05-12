a4232d6a659e1f3feb12b245b2e912c28e0b1009
Tony <saric.tony@gmail.com>
2017-05-12 14:33:20 -0700

    reloads files

--- db/schema.rb
+++ db/schema.rb
@@ -32,7 +32,6 @@ ActiveRecord::Schema.define(version: 20170329223559) do
     t.integer  "prep_time"
     t.string   "category"
     t.text     "instructions"
-    t.decimal  "votes"
     t.string   "image_file_name"
     t.string   "image_content_type"
     t.integer  "image_file_size"
